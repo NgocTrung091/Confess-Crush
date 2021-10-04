f38f11b8226aa159986d5e46f6fd2e013410cc91
NgocTrung091 <91879374+NgocTrung091@users.noreply.github.com>
2021-10-04 19:32:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^BẤM VÔ BẤM VÔ^^',
+    title: 'Em thấy anh đẹp trai, nên em yêu anh lắm chứ gì 🥰',
+    desc: 'Nói thật điii, thích anh lắm hả ?? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Em định bấm <không nha>, mà không được chứ gì :((',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Không phải mà, em thích anh thật á, yêu anh nữa',
+    mess: 'Anh biết mà 🥰. Yêu emmm nhiều lắm á 😘😘',
+    messDesc: 'Đưng bỏ rơi tui nha :((',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
